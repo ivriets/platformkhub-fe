9d402f883b6f6e69ba39501351ef7634ea47904d
stevengerrad57 <stevengerrad57@gmail.com>
2022-11-02 10:18:50 +0700

    tes to vercel

--- components/dashboard/individu/detail.vue
+++ components/dashboard/individu/detail.vue
@@ -56,6 +56,7 @@
                                             <div>Facebook</div>
                                             <div>Instagram</div>
                                             <div>LinkedIn</div>
+                                            <div>Youtube</div>
                                         </div>
                                     </div>
                                 </div>
