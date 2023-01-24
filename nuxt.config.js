a84c4dbb1315ee8a65b085d62908bab74c96083d
ivriets <one.grip07@gmail.com>
2023-01-25 01:44:46 +0700

    server

--- nuxt.config.js
+++ nuxt.config.js
@@ -113,10 +113,10 @@ export default {
 
     }],
   ],
-  // server: {
-  //   host: "0.0.0.0",
-  //   port: 3333,
-  // },
+  server: {
+    host: "0.0.0.0",
+    port: 3333,
+  },
   dayjs: {
     locales: ['id'],
     defaultLocale: 'id',
