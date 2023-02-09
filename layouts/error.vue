20b89ae77b7cd14c5c693338c8a49fcf935daaec
ivriets <one.grip07@gmail.com>
2023-02-10 04:52:08 +0700

    aw error

--- layouts/error.vue
+++ layouts/error.vue
@@ -6,28 +6,6 @@
           </h1>
         <img src="/images/error.png" class="w-1/2 h-auto mx-auto" alt="error" />
         </div>
-
-
-      <!-- <div
-        id="sky"
-        class="fixed z-50 top-0 h-screen w-screen flex justify-center items-center"
-      >
-        <div
-          class="hidden flex-col items-center justify-center bg-gray-900 text-white w-screen h-screen md:flex"
-        >
-          <div id="stars" class="self-start z-30"></div>
-          <div id="stars2" class="self-start z-40"></div>
-          <div id="stars3" class="self-start z-20"></div>
-          <h1 class="font-mono text-white z-50 text-6xl">
-            {{ error.statusCode }} - Ooops... are you lost?
-          </h1>
-          <nuxt-link
-            to="/"
-            class="space cursor-pointer z-50 flex-col justify-center items-center flex pt-4 px-8 rounded-md text-purple-800 transform duration-150 ease-in hover:text-white hover:scale-105"
-          >
-          </nuxt-link>
-        </div>
-      </div> -->
       
     </div>
   </template>
